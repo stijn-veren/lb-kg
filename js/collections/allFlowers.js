55c39f7884384af43916782f716e88fc4ffd3de9
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-26 22:22:16 +0300

    Add comments to allFlowers.js

--- js/collections/allFlowers.js
+++ js/collections/allFlowers.js
@@ -1,6 +1,8 @@
 // Namespace our flowerApp
 var app = app || {}
 
+// A group (array) of Flower models
 app.FlowersCollection = Backbone.Collection.extend({
+  // What type of models are in this collection?
   model: app.singleFlower,
 })
